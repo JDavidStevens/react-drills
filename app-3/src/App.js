1adefb5d8100de0ccfbafe5e6780e0a65b47af93
unknown <jdavid.stevens@gmail.com>
2018-07-03 08:14:31 -0600

    react2.1

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,17 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+constructor(){
+  super()
+
+  this.state={
+    filterString: "",
+    name: ["Jimmy","John","Robert","Bonzo"]
+  }
+}
+  handleChange(filter){
+    this.setState({filterString : filter})
+  
+}
   render() {
+    let listOfNames  = this.state.name.filter((element,index)=>{
+    return element.includes(this.state.filterString);
+    }).map((element,index)=> {
+      return <h2 key={index}>{element}</h2>
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <input onChange={(e) => this.handleChange(e.target.value)} type="text" />
+      {listOfNames}
+  </div>
     );
   }
 }
